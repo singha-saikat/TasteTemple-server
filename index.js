4dc1eb02a2ece6a5c93aa812137e8a748a4193ac
Saikat <sachinsingha7899@gmail.com>
2023-10-19 02:15:56 +0600

    Using get loaded brand fake data and display in fronend

--- index.js
+++ index.js
@@ -25,38 +25,45 @@ const client = new MongoClient(uri, {
   }
 });
 
-async function insertFakeData() {
-  try {
-    await client.connect();
-    const newCollection = client.db("productDb").collection("Brand");
+// async function insertFakeData() {
+//   try {
+//     await client.connect();
+//     const newCollection = client.db("productDb").collection("Brand");
+
+//     //  get brand fake
+//     app.get("/brand", async(req,res)=>{
+//       const result = await newCollection.find().toArray();
+//       res.send(result)
+//     })
     
 
-    const brandData = [
+//     const brandData = [
       
-        { "name": "Starbucks", "brandImage": "https://i.postimg.cc/8ChsJHqp/Starbucks-Talky.jpg" },
-        { "name": "McDonald's", "brandImage": "https://i.postimg.cc/kGZtF5wq/Mc-Donald-s.jpg" },
-        { "name": "PepsiCo", "brandImage": "https://i.postimg.cc/Bt9nnPML/pepsico.png" },
-        { "name": "Nestle", "brandImage": "https://i.postimg.cc/bJpMpyxy/Nestle.png" },
-        { "name": "Kellogg's", "brandImage": "https://i.postimg.cc/8ztQcrCF/Kellogg-s.webp" },
-        { "name": "Pran", "brandImage": "https://i.postimg.cc/25kkXD1W/pran.jpg" }
+//         { "name": "Starbucks", "brandImage": "https://i.postimg.cc/8ChsJHqp/Starbucks-Talky.jpg" },
+//         { "name": "McDonald's", "brandImage": "https://i.postimg.cc/kGZtF5wq/Mc-Donald-s.jpg" },
+//         { "name": "PepsiCo", "brandImage": "https://i.postimg.cc/Bt9nnPML/pepsico.png" },
+//         { "name": "Nestle", "brandImage": "https://i.postimg.cc/bJpMpyxy/Nestle.png" },
+//         { "name": "Kellogg's", "brandImage": "https://i.postimg.cc/8ztQcrCF/Kellogg-s.webp" },
+//         { "name": "Pran", "brandImage": "https://i.postimg.cc/25kkXD1W/pran.jpg" }
       
       
-    ];
+//     ];
 
-    const result = await newCollection.insertMany(brandData);
-    console.log(`${result.insertedCount} documents inserted`);
-  } finally {
-    client.close();
-  }
-}
+//     const result = await newCollection.insertMany(brandData);
+//     console.log(`${result.insertedCount} documents inserted`);
+//   } finally {
+//     client.close();
+//   }
+// }
 
-insertFakeData();
+// insertFakeData();
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const productCollection = client.db('productDb').collection("product")
+    const newCollection = client.db("productDb").collection("Brand");
 
     // post method applied here 
     app.post('/products',async (req,res) =>{
@@ -64,6 +71,14 @@ async function run() {
       const result = await productCollection.insertOne(product);
       res.send(result) 
     })
+
+    app.get("/brands", async (req, res) => {
+      const result = await newCollection.find().toArray();
+      res.send(result);
+    });
+    
+    
+
 
 
     // Send a ping to confirm a successful connection
