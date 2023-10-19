74fe8720dbaf773cd69c9762a17ed8dc9232dbd6
Saikat <sachinsingha7899@gmail.com>
2023-10-20 03:07:40 +0600

    make details button functional

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { ObjectId } = require("mongodb");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
-const port = process.env.PORT || 4000 ;
-
+const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
 // TasteTemple
 // 7GjiYyYClmK9XbEv
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ggrwjrl.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -22,62 +21,32 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
-// async function insertFakeData() {
-//   try {
-//     await client.connect();
-//     const newCollection = client.db("productDb").collection("Brand");
 
-//     //  get brand fake
-//     app.get("/brand", async(req,res)=>{
-//       const result = await newCollection.find().toArray();
-//       res.send(result)
-//     })
-    
-
-//     const brandData = [
-      
-//         { "name": "Starbucks", "brandImage": "https://i.postimg.cc/8ChsJHqp/Starbucks-Talky.jpg" },
-//         { "name": "McDonald's", "brandImage": "https://i.postimg.cc/kGZtF5wq/Mc-Donald-s.jpg" },
-//         { "name": "PepsiCo", "brandImage": "https://i.postimg.cc/Bt9nnPML/pepsico.png" },
-//         { "name": "Nestle", "brandImage": "https://i.postimg.cc/bJpMpyxy/Nestle.png" },
-//         { "name": "Kellogg's", "brandImage": "https://i.postimg.cc/8ztQcrCF/Kellogg-s.webp" },
-//         { "name": "Pran", "brandImage": "https://i.postimg.cc/25kkXD1W/pran.jpg" }
-      
-      
-//     ];
-
-//     const result = await newCollection.insertMany(brandData);
-//     console.log(`${result.insertedCount} documents inserted`);
-//   } finally {
-//     client.close();
-//   }
-// }
-
-// insertFakeData();
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const productCollection = client.db('productDb').collection("product")
+    const productCollection = client.db("productDb").collection("product");
     const newCollection = client.db("productDb").collection("Brand");
+    const brandImageCollection = client
+      .db("productDb")
+      .collection("BrandImage");
 
-    // post method applied here 
-    app.post('/products',async (req,res) =>{
+    // post method applied here
+    app.post("/products", async (req, res) => {
       const product = req.body;
       const result = await productCollection.insertOne(product);
-      res.send(result) 
-    })
+      res.send(result);
+    });
 
     app.get("/brands", async (req, res) => {
       const result = await newCollection.find().toArray();
       res.send(result);
     });
-
-    
 
     app.get("/:brandName", async (req, res) => {
       const brand = req.query.brand;
@@ -88,14 +57,33 @@ async function run() {
       console.log(result);
       res.send(result);
     });
-    
-    
 
+    app.get("/productDetails/:_id", async (req, res) => {
+      const id = req.params._id;
+      const query = {
+        _id: new ObjectId(id),
+      };
+      const result = await productCollection.findOne(query);
+      // console.log(result);
+      res.send(result);
+    });
 
+    // app.get("/:brandName", async (req, res) => {
+    //   const brand = req.params.brandName;
+    //   const query = {
+    //     brand_name: brand,
+    //   };
+    //   console.log("Querying for brand:", brand);
+    //   const result = await brandImageCollection.find(query).toArray();
+    //   console.log("Result:", result);
+    //   res.send(result);
+    // });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -103,13 +91,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/' , (req,res) => {
-    res.send('Taste-Temple server is running ')
+app.get("/", (req, res) => {
+  res.send("Taste-Temple server is running ");
 });
 
-app.listen( port,(req,res) => {
-    console.log(`Taste-Temple server is running on ${port}`)
-})
+app.listen(port, (req, res) => {
+  console.log(`Taste-Temple server is running on ${port}`);
+});
